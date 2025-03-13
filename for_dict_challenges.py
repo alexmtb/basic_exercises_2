d310ba85602c0b548ed418c3a7113d56cb64e9d7
Aleksandr Smirnov <aismirnov@sberbank.ru>
2025-03-13 20:58:39 +0300

    Part of homework 2

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -13,15 +13,25 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
-for student in students:
-    print(f'{student["first_name"]}: {students.count(student)}')
 
+# make dict with name: count of repetitions
+students_count = {}
+for student in students:
+    name = student['first_name']
+    if name in students_count:
+        students_count[name] += 1
+    else:
+        students_count[name] = 1
+
+for name, count in students_count.items():
+    print(f'{name}: {count}')
 
 # Задание 2
 # Дан список учеников, нужно вывести самое часто повторящееся имя
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
+
+print("\nЗадание 2:\n")
 students = [
     {'first_name': 'Вася'},
     {'first_name': 'Петя'},
@@ -30,6 +40,17 @@ students = [
     {'first_name': 'Оля'},
 ]
 # ???
+students_count = {}
+for student in students:
+    name = student['first_name']
+    if name in students_count:
+        students_count[name] += 1   
+    else:
+        students_count[name] = 1
+
+name_mode = max(students_count, key=students_count.get)
+
+print(f"Самое частое имя среди учеников: {name_mode}")
 
 
 # Задание 3
@@ -38,6 +59,7 @@ students = [
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
 
+print("\nЗадание 3:\n")
 school_students = [
     [  # это – первый класс
         {'first_name': 'Вася'},
@@ -54,7 +76,20 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+
+
+for num, classroom in enumerate(school_students, 1):
+    # temp dict for every classroom
+    students_count = {}
+    for student in classroom:
+        name = student['first_name']
+        if name in students_count:
+            students_count[name] += 1   
+        else:
+            students_count[name] = 1
+    name_mode = max(students_count, key=students_count.get)
+    print(f"Самое частое имя в классе № {num}: {name_mode}")
+
 
 
 # Задание 4
@@ -62,6 +97,8 @@ school_students = [
 # Пример вывода:
 # Класс 2a: девочки 2, мальчики 0 
 # Класс 2б: девочки 0, мальчики 2
+
+print("\nЗадание 4:\n")
 
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
@@ -76,6 +113,12 @@ is_male = {
     'Даша': False,
 }
 # ???
+print("\nЗадание 4:\n")
+# list comprehension for girls and boys in every class
+for i, s_class in enumerate(school, 1):
+    girls_count = len([name for name in s_class['students'] if not is_male[name['first_name']]])
+    boys_count = len([name for name in s_class['students'] if is_male[name['first_name']]])
+    print(f"Класс {s_class['class']}: девочки {girls_count}, мальчики {boys_count}")
 
 
 # Задание 5
@@ -84,6 +127,7 @@ is_male = {
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
 
+print("\nЗадание 5:\n")
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
@@ -95,4 +139,12 @@ is_male = {
     'Миша': True,
 }
 # ???
+for s_class in school:
+    girls_count = len([name for name in s_class['students'] if not is_male[name['first_name']]])
+    boys_count = len([name for name in s_class['students'] if is_male[name['first_name']]])
+    if girls_count > boys_count:
+        print(f"Больше всего девочек в классе {s_class['class']}")
+    elif girls_count < boys_count:
+        print(f"Больше всего мальчиков в классе {s_class['class']}")
+        
 
--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -22,8 +22,8 @@ messages = [
 Установите библиотеку lorem, чтобы она работала.
 
 Нужно:
-1. Вывести айди пользователя, который написал больше всех сообщений.
-2. Вывести айди пользователя, на сообщения которого больше всего отвечали.
+1. Вывести айди пользователя, который написал больше всех сообщений. # done
+2. Вывести айди пользователя, на сообщения которого больше всего отвечали. # done
 3. Вывести айди пользователей, сообщения которых видело больше всего уникальных пользователей.
 4. Определить, когда в чате больше всего сообщений: утром (до 12 часов), днём (12-18 часов) или вечером (после 18 часов).
 5. Вывести идентификаторы сообщений, который стали началом для самых длинных тредов (цепочек ответов).
@@ -33,10 +33,10 @@ messages = [
 import random
 import uuid
 import datetime
-
+import pprint as pp
 import lorem
 
-
+random.seed(42)
 def generate_chat_history():
     messages_amount = random.randint(200, 1000)
     users_ids = list(
@@ -44,6 +44,7 @@ def generate_chat_history():
     )
     sent_at = datetime.datetime.now() - datetime.timedelta(days=100)
     messages = []
+    
     for _ in range(messages_amount):
         sent_at += datetime.timedelta(minutes=random.randint(0, 240))
         messages.append({
@@ -65,6 +66,44 @@ def generate_chat_history():
         })
     return messages
 
+# id пользователя, который написал больше всех сообщений
+def max_messages(messages):
+    user_messages = {message["sent_by"]: 0 for message in messages}
+    for message in messages:
+        user_messages[message["sent_by"]] += 1
+    return max(user_messages, key=user_messages.get)
+
+# id пользователя, на сообщения которого больше всего отвечали.
+def max_reply(messages):
+    reply_messages = {message["reply_for"]: 0 for message in messages}
+    for message in messages:
+        if message["reply_for"]:
+            reply_messages[message["reply_for"]] += 1
+    id = max(reply_messages, key=reply_messages.get)
+    id_count = reply_messages[id]
+    return id, id_count
+
+# id пользователей, сообщения которых видело больше всего уникальных пользователей.
+def max_seen(messages):
+    seen_by = {message["id"]: [] for message in messages}
+    for message in messages:
+        seen_by[message["id"]].append(message["seen_by"])
+    user_messages = {user: 0 for user in users_ids}
+    for message in messages:
+        if message["reply_for"]:
+            user_messages[message["sent_by"]] += 1
+    return max(user_messages, key=user_messages.get)
+
+
+
+
+all_messages = generate_chat_history()
+id_max_messages = max_messages(all_messages)
+id_max_replies = max_reply(all_messages)
+id_unique_max = max_seen(all_messages)
+print(f'id пользователя, который написал больше всех сообщений: {id_max_messages}')
+print(f'id пользователя, на сообщения которого больше всего отвечали: {id_max_replies[0]} (ответов: {id_max_replies[1]})')
+print(f'id пользователей, сообщения которых видело больше всего уникальных пользователей: {id_unique_max}')
 
 if __name__ == "__main__":
-    print(generate_chat_history())
+    pp.pprint(generate_chat_history()[-10])
